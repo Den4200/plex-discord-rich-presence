90c5973f61587b4b263f493c4610c24c8713e2cf
Den4200 <dennis@dennispham.me>
2020-08-07 16:55:36 -0400

    Create base PlexDiscordRichPresence client

---
+++ plex_drp/rich_presence.py
@@ -0,0 +1,58 @@
+import asyncio
+import logging
+import typing as t
+
+from plexapi.myplex import MyPlexAccount
+from pypresence import Presence
+
+log = logging.getLogger(__name__)
+
+
+class PlexDiscordRichPresence(Presence):
+
+    def __init__(
+        self,
+        username: str,
+        password: t.Optional[str] = None,
+        token: t.Optional[str] = None,
+        client_id: str = '741382142730305587'
+    ) -> None:
+        super().__init__(client_id=client_id, loop=asyncio.get_event_loop())
+
+        self.username = username
+
+        if not password and not token:
+            raise RuntimeError('Password and token are both empty! Please provide at least one.')
+
+        self.password = password
+        self.token = token
+
+        self.plex_account = None
+
+        self.connected = asyncio.Event()
+
+    async def connect(self) -> None:
+        while True:
+            try:
+                if self.token:
+                    self.plex_account = MyPlexAccount(self.username, token=self.token)
+                else:
+                    self.plex_account = MyPlexAccount(self.username, self.password)
+
+                log.info(f'Logged into Plex as {self.username}.')
+                break
+
+            except Exception as err:
+                log.error(f'Failed to connect to Plex: {err}')
+                log.debug('Attempting reconnection in 10 seconds..')
+
+                await asyncio.sleep(10)
+
+        log.debug('Attempting to open IPC connection to Discord..')
+        await self.handshake()
+        log.info('IPC connection established to Discord.')
+
+        self.connected.set()
+
+    def run(self) -> None:
+        self.loop.run_until_complete(self.connect())
